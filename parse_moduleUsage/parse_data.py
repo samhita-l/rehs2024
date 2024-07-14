a08a8c49e6c9b3585970bd18ccededa5bacddecf
Ethjin8 <ethjin8@gmail.com>
2024-07-14 12:20:19 -0700

    1. Added ability to display data in bar graphs using matplotlib
    2. Streamlined main() function, built plot_data() and print_data() functions

--- parse_moduleUsage/parse_data.py
+++ parse_moduleUsage/parse_data.py
@@ -16,8 +16,11 @@ def parse_user_input():
 
     parser.add_argument('-s', '--save', action="store_true", help='Save dataframe to file')
     parser.add_argument('-t', '--filetype', type=str, default='csv', nargs='+', choices=['csv', 'parquet'], help='File type to save dataframe')
+
     parser.add_argument('-m', '--unique_modules', action="store_true", help='Determine number of unique modules and how many times each one appears')
+    parser.add_argument('-pltm', '--plot_modules', action="store_true", help='Plot bar graph of unique modules')
     parser.add_argument('-u', '--unique_users', action="store_true", help='Determine number of unique users and how many times each one appears')
+    parser.add_argument('-pltu', '--plot_users', action="store_true", help='Plot bar graph of unique users')
 
     args = parser.parse_args()
     return args
@@ -94,6 +97,19 @@ def save_dataframe(df, args):
     if ('parquet' in args.filetype):
         df.to_parquet('moduleUsage.parquet')
 
+def plot_data(data, series):
+    plt.bar(range(5), series[:5], align='center', alpha=0.8)
+    plt.xlabel(data)
+    plt.ylabel('Frequency')
+    plt.title('Top 5 ' + data)
+    plt.xticks(range(5), series.index[:5])  # Set x-axis labels to match index values
+    plt.show()
+
+def print_data(data, series):
+    print(series)
+    print("\nThere are " + str(len(series)) + " unique " + data + "s. The complete list is shown above, ranked by how frequently each " + data + " appears.\n")
+    print("The most frequent " + data + " is " + series.idxmax() + " with " + str(series.max()) + " occurrences.\n")
+
 
 def main():
     args = parse_user_input()
@@ -103,18 +119,19 @@ def main():
         save_dataframe(df, args)
     elif (args.unique_modules):
         unique_modules = df['Module'].value_counts()
-        print(unique_modules)
-        print("\nThere are " + str(len(unique_modules)) + " unique modules. The complete list is shown above, ranked by how frequently each user appears.\n")
-        print("The most frequently used module is " + unique_modules.idxmax() + " with " + str(unique_modules.max()) + " uses.\n")
+        if (args.plot_modules):
+            plot_data('Modules', unique_modules)
+        else:
+            print_data('module', unique_modules)
     elif (args.unique_users):
         unique_users = df['Username'].value_counts()
-        print(unique_users)
-        print("\nThere are " + str(len(unique_users)) + " unique users. The complete list is shown above, ranked by how frequently each user appears.\n")
-        print("The most frequent user is " + unique_users.idxmax() + " with " + str(unique_users.max()) + " appearances.\n")
+        if (args.plot_users):
+            plot_data('Users', unique_users)
+        else:
+            print_data('user', unique_users)
     else:
         print(df)
 
-    
     
 if __name__ == "__main__":
     main()
